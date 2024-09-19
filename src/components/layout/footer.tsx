8f6abc971d2d7f13ea2a8313302a130d8b16e712
Weng Wong (George) Hum <124537582+wengwongg@users.noreply.github.com>
2024-09-19 15:47:58 +0100

    added new office.

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -13,49 +13,65 @@ export default function Footer() {
           <FaLinkedin size={30} />
         </div>
       </div>
-      <div className="flex flex-col sm:flex-row gap-6 sm:gap-10">
-        <div className="flex flex-col">
-          <h5 className="font-bold mb-2">Navigation</h5>
-          {navItems.map((item) => (
-            <Link key={item.name} href={item.href}>
-              {item.name}
-            </Link>
-          ))}
+      <div className="flex flex-col sm:flex-row gap-6">
+        <div className="flex flex-col gap-6 sm:flex-row">
+          <div className="flex flex-col">
+            <h5 className="font-bold mb-2">Navigation</h5>
+            {navItems.map((item) => (
+              <Link key={item.name} href={item.href}>
+                {item.name}
+              </Link>
+            ))}
+          </div>
+          <div className="flex flex-col">
+            <h5 className="font-bold mb-2">Main office</h5>
+            <p>
+              Lorong Mak Mandin 4,
+              <br />
+              Kawasan Perindustrian
+              <br />
+              Mak Mandin,
+              <br />
+              13400 Butterworth,
+              <br />
+              Pulau Pinang
+            </p>
+          </div>
+          <div className="flex flex-col">
+            <h5 className="font-bold mb-2">Second office</h5>
+            <p>
+              No. 15 Jalan Sega Jaya 1,
+              <br />
+              Taman Perindustrian Saga Jaya
+              <br />
+              13600 Perai,
+              <br />
+              Pulau Pinang
+            </p>
+          </div>
         </div>
-        <div className="flex flex-col">
-          <h5 className="font-bold mb-2">Main office</h5>
-          <p>
-            Lorong Mak Mandin 4,
-            <br />
-            Kawasan Perindustrian
-            <br />
-            Mak Mandin,
-            <br />
-            13400 Butterworth,
-            <br />
-            Pulau Pinang
-          </p>
-        </div>
-        <div className="flex flex-col">
-          <h5 className="font-bold mb-2">LBS Industries</h5>
-          <p>
-            27, Lintang Bayan Lepas 6,
-            <br />
-            Bayan Lepas Free
-            <br />
-            Industrial Zone Phase 4,
-            <br />
-            11900 Bayan Lepas,
-            <br />
-            Pulau Pinang
-          </p>
-        </div>
-        <div className="flex flex-col">
-          <h5 className="font-bold mb-2">Email</h5>
-          <p className="mb-5">rakankertas2@gmail.com</p>
+        <div className="flex flex-col gap-6 lg:flex-row">
+          <div className="flex flex-col">
+            <h5 className="font-bold mb-2">LBS Industries</h5>
+            <p>
+              27, Lintang Bayan Lepas 6,
+              <br />
+              Bayan Lepas Free
+              <br />
+              Industrial Zone Phase 4,
+              <br />
+              11900 Bayan Lepas,
+              <br />
+              Pulau Pinang
+            </p>
+          </div>
+          <div className="flex flex-col">
+            <h5 className="font-bold mb-2">Email</h5>
+            <p className="mb-6">rakankertas2@gmail.com</p>
 
-          <h5 className="font-bold mb-2">Phone</h5>
-          <p>04-3232273 / 04-6465573</p>
+            <h5 className="font-bold mb-2">Phone</h5>
+            <p>04-3232273 / 04-6465573</p>
+          </div>
         </div>
       </div>
     </footer>
